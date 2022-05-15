7991c64feb6bf0ab8e71194e4d745d1b288cfd63
Owen Kraweki <owenkraweki@post.cz>
2022-05-15 16:04:35 +0200

    Reformat Java code

--- src/main/java/org/chernovia/molechess/MoleListener.java
+++ src/main/java/org/chernovia/molechess/MoleListener.java
@@ -3,9 +3,13 @@ package org.chernovia.molechess;
 import java.util.ArrayList;
 
 public interface MoleListener {
-	public void started(MoleGame game);
-	public void updateUserData(ArrayList<MolePlayer> winners, ArrayList<MolePlayer> losers, boolean draw);
-	public void updateUser(MoleUser user, MoleGame game, MoleResult action, boolean movelist);
-	public void updateGame(MoleGame game, MoleResult action, boolean movelist);
-	public void finished(MoleGame game);
+    public void started(MoleGame game);
+
+    public void updateUserData(ArrayList<MolePlayer> winners, ArrayList<MolePlayer> losers, boolean draw);
+
+    public void updateUser(MoleUser user, MoleGame game, MoleResult action, boolean movelist);
+
+    public void updateGame(MoleGame game, MoleResult action, boolean movelist);
+
+    public void finished(MoleGame game);
 }
